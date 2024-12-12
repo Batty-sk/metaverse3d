e4507717e6240e5ae44cd35d0a50fe6828ff5273
Batty-sk <saorav.skumar@gmail.com>
2024-12-12 15:51:23 +0530

    added: established webrtc connection.

--- src/contexts/Socket.tsx
+++ src/contexts/Socket.tsx
@@ -43,6 +43,9 @@ export const SocketContext = createContext<
 type socketContextWrapperProps = {
   children: ReactNode;
 };
+type peerJoinsProp = {
+  peerID: string;
+};
 
 export const SocketContextWrapper = ({
   children,
@@ -51,13 +54,12 @@ export const SocketContextWrapper = ({
 
   const [users] = useState(); //it will store all the users socketids.. and
 
-  const peers=useRef<{socketId:Peer}>() 
-  const myPeer = useRef<Peer>()
+  const peers = useRef<{ socketId: Peer }>();
+  const myPeer = useRef<Peer>();
 
   useEffect(() => {
     console.log("sendiing the sockets request to the socket  io server ..... ");
     updateSocket(io("http://localhost:8080")); // for the testing purpose...
-  
   }, []);
 
   useEffect(() => {
@@ -65,10 +67,45 @@ export const SocketContextWrapper = ({
       socket.emit("fetchPlayers", () => {
         console.log("players fetched ...");
       });
+      console.log("socketid", socket);
+      socket.on("connect", () => {
+        console.log("Socket connected, id:", socket.id);
+        if (socket.id) {
+          myPeer.current = new Peer(socket.id); //  Create a Peer with socketId as its unique identifier
+          myPeer.current?.addListener("open", (id) => {
+            console.log(
+              "Connection has been opened! of my peer sending my availability to otherss..",
+              id
+            );
+            socket.emit("registerMe", { peerID: id });
+          });
+          myPeer.current.addListener("connection", (connection) => {
 
-      if(socket.id)
-      myPeer.current = new Peer(socket.id) //create a Peer with socketId as its unique identifier
+            console.log('connection request coming from',connection.peer)
+            connection.on("open", () => {
+              console.log("Connection opened with peer", connection.peer);
+              connection.send("Hi, I'm your new friend!");
+            });
+  
+  
+            connection.on("data", (data) => {
+              console.log("Received data from peer:", data); // Logs the data received from the peer
+            });
+            // Optionally handle when the connection is closed
+            connection.on("close", () => {
+              console.log("Connection closed with peer", connection.peer);
+            });
 
+            console.log(
+              "device which is connected with has the connection",
+              connection
+            );
+            console.log('sending the data to the connection')
+          });
+        }
+
+        socket.on("someone-joins", handleSomeoneJoins);
+      });
       // will create a offer regarding the current specification my
       // device supports. but we need to create an offer everytime for each socket/user.
 
@@ -79,17 +116,34 @@ export const SocketContextWrapper = ({
 
       socket?.on("messageRequest", handleMessageRequest); // for handling the message request or we can say the message
       // sent by the friend..  this event will recieve that message.
-
-    
-
     }
     return () => {
       socket?.off("messageRequest", handleMessageRequest);
+      socket?.off("someone-joins", handleSomeoneJoins);
     };
   }, [socket]);
-    
-  
- 
+
+  type peerJoinsProp = {
+    peerID: string;
+  };
+  const handleSomeoneJoins = (peerID: string) => {
+    console.log("peer joins with peerId ", peerID);
+    const connectionToSomeone = myPeer.current?.connect(peerID);
+    if (connectionToSomeone) {
+      connectionToSomeone.on("open", () => {
+        console.log("Connection established with peer ", peerID);
+        // You can now start sending data or setting up media streams
+      });
+
+      connectionToSomeone.on("data", (data) => {
+        console.log("Received data from peer:", data);
+      });
+
+      connectionToSomeone.on("close", () => {
+        console.log("Connection closed with peer", peerID);
+      });
+    }
+  };
 
   const handleMessageRequest = () => {};
   return (
@@ -100,7 +154,7 @@ export const SocketContextWrapper = ({
         noOfPlayers: 0,
         players: [],
         messages: [],
-        fetchCurrentUsersInTheLobby: /* fetchCurrentUsersInTheLobby */ ()=>0,
+        fetchCurrentUsersInTheLobby: /* fetchCurrentUsersInTheLobby */ () => 0,
       }}
     >
       {children}
